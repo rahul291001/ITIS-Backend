63b67d571fc0fb9f17241747283890215666e5b5
rahul291001 <rahul.s.goud@gmail.com>
2024-05-09 13:47:15 +1000

    did the backend logic

--- controllers/usercontroller.js
+++ controllers/usercontroller.js
@@ -111,6 +111,7 @@ export const login = async (req, res) => {
       path: "/",
       maxAge: 1000 * 60 * 60 * 24 * 365,
     });
+    
 
     res.status(200).json({ success: true, user: findUser });
   } catch (error) {
